ab0fe4e5cea82125f3bea0f436e9038d5540f211
Maheanuu Allain <maheanuu.allain@gmail.com>
2024-09-21 14:16:45 +0200

    refactor: change redirection to home page after creation

--- app/Controllers/home.php
+++ app/Controllers/home.php
@@ -10,47 +10,32 @@ class Home
 {
     public function __construct()
     {
-        define('BASE_DIR', __DIR__ . '/..');
+        define("BASE_DIR", __DIR__ . "/..");
     }
 
     // Affiche la page d'accueil
     public function index(): void
     {
         if ($_SERVER["REQUEST_URI"] === "/") {
-            if (empty(file_get_contents(__DIR__ . "/../config/config.php"))) {
+            if (
+                file_exists(__DIR__ . "/../config/config.php") &&
+                !empty(file_get_contents(__DIR__ . "/../config/config.php"))
+            ) {
+                $Page = new Page();
+                $homePage = $Page->getOneBy(["url" => "/home"]);
+
+                if ($homePage) {
+                    $pageData = $homePage[0];
+                    include __DIR__ . "/../Views/front-office/page/page.php";
+                } else {
+                    include __DIR__ . "/../Views/front-office/main/home.php";
+                }
+            } else {
                 include __DIR__ . "/../Views/front-office/main/home.php";
-            }
-            else {
-                include __DIR__ . "/../Views/back-office/installer/installer_loginAdmin.php";
             }
         } else {
             $Error = new Error();
             $Error->error404();
         }
     }
-
-    // Affiche la page de l'utilisateur
-    public function mypage($uri = ''): bool
-    {
-        $Page = new Page();
-
-        $Data = $Page->getAll();
-
-        $pageData = null;
-
-        foreach ($Data as $page) {
-            if ($page['url'] === $uri) {
-                $pageData = $page;
-                break;
-            }
-        }
-
-        if (!$pageData) {
-            return false;
-        } else {
-            include __DIR__ . '/../Views/front-office/page/page.php';
-            return true;
-        }
-    }
-
 }
